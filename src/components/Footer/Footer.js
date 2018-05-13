a0bdc2e6e810df7069c5df5376a9ef2441823b34
nljacob <neehaljacob@gmail.com>
2018-05-13 13:52:10 -0700

    set up initial files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = (props) => (
+	<footer class="footer">
+		<div class="container">
+			<p class="text-muted">Place sticky footer content here.</p>
+		</div>
+	</footer>
+)
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = (props) => (
+	<nav className="navbar navbar-default">
+		<img alt="Brand" src="..."/>			
+	</nav>
+)
+
+export default Navbar;
