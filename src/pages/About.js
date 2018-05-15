7491b3b309849bc58f38e6d6ef87a80b6e68957e
Johana Ramirez <JCRamirez@Johanas-MacBook-Pro.local>
2018-05-15 11:14:21 -0700

    added questions page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 
-const Home = () => (
+const About = () => (
     <div>
         <Container style={{ marginTop: 30 }}>
             <Row>
@@ -55,4 +55,4 @@ const Home = () => (
     </div>
 );
 
-export default Home;
+export default About;
