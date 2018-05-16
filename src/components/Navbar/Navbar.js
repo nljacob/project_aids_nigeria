066481c715c90ea92d80d176480cd1c6f894b79c
Johana Ramirez <JCRamirez@Johanas-MacBook-Pro.local>
2018-05-16 14:31:01 -0700

    navbar collapse

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,13 @@ import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => (
-  <nav id="nav-bar" className="navbar navbar-expand-lg navbar-light bg-light">
-    <div>
-      <ul className="navbar-nav">
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+   <a className="navbar-brand" href=""></a> 
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
         <li
           className={
             window.location.pathname === "/" ||
@@ -19,7 +23,7 @@ const Navbar = props => (
             Home
           </Link>
         </li>
-        <li
+        <li 
           className={
             window.location.pathname === "/about"
               ? "nav-item active"
@@ -30,7 +34,7 @@ const Navbar = props => (
             About
           </Link>
         </li>
-        <li
+        <li 
           className={
             window.location.pathname === "/questions"
               ? "nav-item active"
@@ -41,7 +45,7 @@ const Navbar = props => (
             F.A.Q
           </Link>
         </li>
-        <li
+        <li 
           className={
             window.location.pathname === "/donate"
               ? "nav-item active"
@@ -52,7 +56,7 @@ const Navbar = props => (
             Donate
           </Link>
         </li>
-        <li
+        <li 
           className={
             window.location.pathname === "/blog"
               ? "nav-item active"
@@ -63,7 +67,7 @@ const Navbar = props => (
             Blog
           </Link>
         </li>
-        <li
+        <li 
           className={
             window.location.pathname === "/students"
               ? "nav-item active"
@@ -74,7 +78,7 @@ const Navbar = props => (
             Our Students
           </Link>
         </li> 
-        <li
+        <li 
           className={
             window.location.pathname === "/curriculum"
               ? "nav-item active"
@@ -87,7 +91,92 @@ const Navbar = props => (
         </li>
       </ul>
     </div>
-  </nav>
+</nav>
+
+  // <nav id="nav-bar" className="navbar navbar-expand-lg navbar-light bg-light">
+  //   <div>
+  //     <ul className="navbar-nav">
+  //       <li
+  //         className={
+  //           window.location.pathname === "/" ||
+  //             window.location.pathname === "/home"
+  //             ? "nav-item active"
+  //             : "nav-item"
+  //         }
+  //       >
+  //         <Link to="/" className="nav-link">
+  //           Home
+  //         </Link>
+  //       </li>
+  //       <li
+  //         className={
+  //           window.location.pathname === "/about"
+  //             ? "nav-item active"
+  //             : "nav-item"
+  //         }
+  //       >
+  //         <Link to="/about" className="nav-link">
+  //           About
+  //         </Link>
+  //       </li>
+  //       <li
+  //         className={
+  //           window.location.pathname === "/questions"
+  //             ? "nav-item active"
+  //             : "nav-item"
+  //         }
+  //       >
+  //         <Link to="/questions" className="nav-link">
+  //           F.A.Q
+  //         </Link>
+  //       </li>
+  //       <li
+  //         className={
+  //           window.location.pathname === "/donate"
+  //             ? "nav-item active"
+  //             : "nav-item"
+  //         }
+  //       >
+  //         <Link to="/donate" className="nav-link">
+  //           Donate
+  //         </Link>
+  //       </li>
+  //       <li
+  //         className={
+  //           window.location.pathname === "/blog"
+  //             ? "nav-item active"
+  //             : "nav-item"
+  //         }
+  //       >
+  //         <Link to="/blog" className="nav-link">
+  //           Blog
+  //         </Link>
+  //       </li>
+  //       <li
+  //         className={
+  //           window.location.pathname === "/students"
+  //             ? "nav-item active"
+  //             : "nav-item"
+  //         }
+  //       >
+  //         <Link to="/students" className="nav-link">
+  //           Our Students
+  //         </Link>
+  //       </li> 
+  //       <li
+  //         className={
+  //           window.location.pathname === "/curriculum"
+  //             ? "nav-item active"
+  //             : "nav-item"
+  //         }
+  //       >
+  //         <Link to="/curriculum" className="nav-link">
+  //           Curriculum
+  //         </Link>
+  //       </li>
+  //     </ul>
+  //   </div>
+  // </nav>
 );
 
 export default Navbar;
