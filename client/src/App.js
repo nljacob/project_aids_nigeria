ab1804104aa754a8ab02a27301c5fc1db15c88c7
nkand84 <nityabhagi@gmail.com>
2018-05-17 14:36:16 -0700

    merged user auth with current project..work in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,49 @@
-import React from "react";
+import React, { Component } from 'react';
+import AuthService from './components/AuthService';
+import withAuth from './components/withAuth';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Donate from "./pages/Donate/Donate";
-import Questions from "./pages/Questions/Questions";
-import Students from "./pages/Students/Students";
-import Blog from "./pages/Blog/Blog";
-import Curriculum from "./pages/Curriculum/Curriculum";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
 
-const App = () => (
-  <Router>
-    <div>
-      <Navbar />
-      <Wrapper>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/donate" component={Donate} />
-        <Route exact path="/questions" component={Questions} />
-        <Route exact path="/students" component={Students} />
-        <Route exact path="/blog" component={Blog} />
-        <Route exact path="/curriculum" component={Curriculum} />
-      </Wrapper>
-      <Footer />
-    </div>
-  </Router>
-);
+const Auth = new AuthService();
+
+
+class App extends Component {
+
+  state = {
+    // userId: this.props.user.id,
+    profileLink: ""
+  };
+
+  componentDidMount() {
+    // const profileLinkURL = `/profile/${this.state.userId}`;
+    // this.setState({
+    //   profileLink: profileLinkURL
+    // });
+  }
+
+  handleLogout = () => {
+    Auth.logout();
+    this.props.history.replace('/signup');
+  };
+
+  goToEditProfile = () => {
+    this.props.history.replace(this.state.profileLink);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* <h2>Welcome {this.props.user.email}</h2> */}
+        </div>
+        <p className="App-intro">
+          <button type="button" className="btn btn-primary" onClick={this.goToEditProfile}>Go to Profile</button>
+          <button type="button" className="btn btn-danger" onClick={this.handleLogout}>Logout</button>
+        </p>
+      </div>
+    );
+  }
+  
+}
 
 export default App;
