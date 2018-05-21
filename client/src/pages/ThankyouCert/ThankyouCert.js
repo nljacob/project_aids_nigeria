8a36135e36ed323cd0961ce61d90a20db4efc2b5
aliperez <alicia.perez@utexas.edu>
2018-05-21 16:07:36 -0700

    got redirect to work on submit for get certified pg

---
+++ client/src/pages/ThankyouCert/ThankyouCert.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+// import "./Curriculum.css";
+
+class ThankyouApp extends Component {
+  render() {
+    return (
+      <div className="curriculum-page">
+      <div className="container">
+        <div className="row">
+          <div className="col text-center">
+          <br/>
+          <br/>
+            <h3>Thank you for your submission!</h3>
+            <br/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <p>You will receive an email from us within two weeks to discuss next steps.  We look forward to working with you to bring PAN to your school!   </p>
+            <br/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center">
+          <a className="btn btn-info" href="/" role="button">HOME</a>&nbsp;
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default ThankyouApp;
