110afec5e22ba04e078c28350c116220e0c3d451
nkand84 <nityabhagi@gmail.com>
2018-05-22 08:06:49 -0700

    profile page intiated

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import withAuth from '../../components/withAuth';
 import API from '../../utils/API';
 import { Link } from 'react-router-dom';
+import "./Profile.css";
 
 class Profile extends Component {
 
@@ -22,10 +23,39 @@ class Profile extends Component {
   render() {
     return (
       <div className="container Profile">
-        <h1>On the profile page!</h1>
-        <p>Username: {this.state.username}</p>
-        <p>Email: {this.state.email}</p>
-        <Link to="/">Go home</Link>
+        <h3 className="text-center special">Admin</h3>
+        <p className = "text-center">Welcome {this.state.username}</p>
+        {/* <p>Email: {this.state.email}</p> */}
+        {/* <Link to="/">Go home</Link> */}
+        <table className="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">FirstName</th>
+      <th scope="col">LastName</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
       </div>
     )
   }
