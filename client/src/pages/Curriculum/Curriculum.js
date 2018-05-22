a9a2c569104e6fa87025aa5a2577e23f2791dbdd
Johana Ramirez <JCRamirez@johanas-mbp.attlocal.net>
2018-05-22 07:43:19 -0700

    changed some button names on curriculum page

--- client/src/pages/Curriculum/Curriculum.js
+++ client/src/pages/Curriculum/Curriculum.js
@@ -34,10 +34,10 @@ class App extends Component {
           <a id="button-space" className="btn btn-info" href="" role="button">HIV PREVENTION</a>
           <a id="button-space" className="btn btn-info" href="" role="button">SIGNS YOU MAY HAVE HIV</a>
           <a id="button-space" className="btn btn-info" href="" role="button">STAGES OF HIV</a>
-          <a id="button-space" className="btn btn-info" href="" role="button">WHAT IS HIV?</a>
-          <a id="button-space" className="btn btn-info" href="" role="button">HIV PREVENTION</a>
-          <a id="button-space" className="btn btn-info" href="" role="button">SIGNS YOU MAY HAVE HIV</a>
-          <a id="button-space" className="btn btn-info" href="" role="button">STAGES OF HIV</a>
+          <a id="button-space" className="btn btn-info" href="" role="button">ARE AIDS AND HIV THE SAME?</a>
+          <a id="button-space" className="btn btn-info" href="" role="button">MEDICINE FOR HIV</a>
+          <a id="button-space" className="btn btn-info" href="" role="button">CURES FOR HIV</a>
+          <a id="button-space" className="btn btn-info" href="" role="button">RESOURCES FOR AIDS</a>
           </div>
         </div>
       </div>
